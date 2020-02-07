f831eaba9826e5d70e03edc10f2a5a3c5a88b820
Daniel <daniel.hjartstrom@gmail.com>
2020-02-07 12:59:59 +0100

    working menu oage (Master Detail)

---
+++ MasterDetailLearning/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterDetailLearning
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+        }
+
+        void Page1_Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+        }
+
+        void Page2_Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new Page2());
+            IsPresented = false;
+        }
+
+    }
+}
